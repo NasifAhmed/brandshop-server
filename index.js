dad8fe9932e28b2d4cfb6295cfac60641da554e1
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-10-26 19:52:22 +0600

    Add getcar

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-//
-//user : assignment10user
-//mongo pass : WKNmRAFE8EWz5g36
-
-const uri =
-    "mongodb+srv://assignment10user:WKNmRAFE8EWz5g36@cluster0.nmxrrle.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nmxrrle.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,6 +20,15 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const carCollection = client.db("carDB").collection("car");
+
+        app.get("/getCar", async (req, res) => {
+            const cursor = carCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log(
@@ -32,7 +36,7 @@ async function run() {
         );
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        //await client.close();
     }
 }
 run().catch(console.dir);
